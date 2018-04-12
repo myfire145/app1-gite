6093a341b23532030477ccc4eb09fe30d55f78fb
solano123 <38224628+solano123@users.noreply.github.com>
2018-04-12 08:34:00 -0500

    1subida
    primera aplicacion
    datos del dispocitivo

---
+++ acciones.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+$(document).ready(function (e){
+	document .addEventListener ("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
+	document.addEventListener ("pause",function(){
+		escribehistoria ('la app se pausó');
+	},false);//pause
+	document.addEventListener("resumen",fuction(){
+		escribehistoria ('la app se reinicio');
+	},false);//resume
+	document.addEventListener("online",fuction(){
+		escribehistoria ('la app se conecto a la red');
+	},false);//se conecto
+	document.addEventListener("offline",fuction(){
+		escribehistoria ('la app se desconecto de la red');
+	},false);//se desconecto
+	
+	
+	},false);//ready device
+	
+	
+});//document
+
+function escribehistoria(acction){
+	$('#eHistoria').append('<li>'+accion+'</li>');
+}
+		
